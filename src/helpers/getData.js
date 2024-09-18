eb4c0845beff1882d5685bb719983da7304f4c9e
Diego Rodriguez <107494750+Diiegor98@users.noreply.github.com>
2024-09-18 16:12:33 -0300

    Delete src/helpers directory

--- src/helpers/getData.js
+++
@@ -1,8 +0,0 @@
-export const getData = () => {
-  return fetch("https://fakestoreapi.com/products").then((res) => res.json());
-};
-
-export const getProductById = (id) => {
-  return fetch(`https://fakestoreapi.com/products/${id}`).then((res) => res.json());
-};
-
